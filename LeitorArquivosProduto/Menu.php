27383e6a6b32cf6d0f12d7adbffe88c564ae5d76
ruano <ruano.martinez@gmail.com>
2014-04-22 22:21:56 -0300

    Implementação escrita arquivo

--- LeitorArquivosProduto/Menu.php
+++ LeitorArquivosProduto/Menu.php
@@ -1,50 +1,29 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Home</title>
-    </head>
-<body>
-<h2>Menu</h2>
+	<h2>Menu</h2>
+	<a href="Produto.php?acao=novo">Adicionar +</a><br/>
 	<?
 		$f = fopen("produtos.txt", "r");
 		$i = 1;
 		while (!feof($f)) { 
 			$arrLinha = explode(',',fgets($f));	
-
-			// echo $arrLinha[0];
-			// echo '</br>';
-			// echo $arrLinha[1];
-			// echo '</br>';
-			// echo $arrLinha[2];
-			// echo '</br>';
 			
 			$codigo = '';
-			$descricao = '';
-			$valor = '';
 			
-			for ($j = 0; j < count($arrLinha); $j++)
+			for ($j = 0; $j < count($arrLinha); $j++)
 			{
 				$arrColuna = explode('=', $arrLinha[$j]);
-				switch ($arrColuna[0]) {
-					case "codigo":
-						$codigo = $arrColuna[1];
-						break;
-					case "descricao":
-						$descricao = $arrColuna[1];
-						break;
-					case "valor":
-						$valor = $arrColuna[1];
-						break;
-					default:
-						echo 'Erro na leitura do arquivo.';
-						break;				
-				}
-			} 			
+				
+				if ($arrColuna[0] == "codigo") 
+				{
+					$codigo = $arrColuna[1];
+				}			
+			}
 			
-			?><a href="Produtos.php?codigo=<?=$codigo?>&descricao=<?=$descricao?>$valor=<?=$valor?>">Produto<?=$i?></a></br><?
+			?>
+			<a href="Produto.php?codigo=<?=$codigo?>">Produto<?=$i?></a></br>
+			<?
 			
 			$i = $i + 1;		
-		}// FIM  while(!feof($f))
+		}
 		fclose($f);
 	?>
 </body>
---
+++ LeitorArquivosProduto/Produto.php
@@ -0,0 +1,109 @@
+<?
+	if (isset($_REQUEST['btnSalvar']))
+	{
+		$codigo = $_REQUEST['txtCodigo'];
+		$nome = $_REQUEST['txtNome'];
+		$valor = $_REQUEST['txtValor'];
+		$descricao = $_REQUEST['txtDescricao'];
+		
+		$conteudo = "\n"."codigo=".$codigo.","."nome=".$nome.","."descricao=".$descricao.","."valor=".$valor;
+		$file = fopen('produtos.txt', 'a');
+		fwrite($file, $conteudo);
+	}
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Produtos</title>
+    </head>
+    <body>
+        <h1>Produtos</h1>
+        
+        <table border="1">
+        	<tr>
+        		<td>
+        			<?include 'Menu.php'?>			
+        		</td> 
+        		       		
+        		<td>
+        			<?
+        				if (isset($_REQUEST['codigo']))
+						{
+							$codigoRequest = $_REQUEST['codigo'];
+							
+							$f = fopen("produtos.txt", "r");
+							while (!feof($f)) 
+							{ 
+								$arrLinha = explode(',',fgets($f));	
+								
+								$codigo = '';
+								$nome = '';
+								$descricao = '';
+								$valor = '';
+								
+								for ($i = 0; $i < count($arrLinha); $i++)
+								{
+									$arrColuna = explode('=', $arrLinha[$i]);
+									
+									switch ($arrColuna[0]) {
+										case 'codigo':
+											$codigo = $arrColuna[1];
+											break;
+										case 'nome':
+											$nome = $arrColuna[1];
+											break;
+										case 'descricao':
+											$descricao = $arrColuna[1];
+											break;
+										case 'valor':
+											$valor = $arrColuna[1];
+											break;										
+										default:											
+											break;
+									}
+								}
+								
+								if ($codigoRequest == $codigo)
+								{
+									?>
+										<form>
+											<label>Código</label><br />
+											<input type="text" name="txtCodigo" value="<?=$codigo?>" /><br />
+											<label>Nome</label><br />
+											<input type="text" name="txtNome" value="<?=$nome?>" /><br />
+											<label>Valor</label><br />
+											<input type="text" name="txtValor" value="<?=$valor?>" /><br />
+											<label>Descrição</label><br />
+											<textarea rows="4" cols="30" name="txtDescricao"><?=$descricao?></textarea>							
+										</form>
+									<?	
+								}			
+							}
+							fclose($f);								
+						} else {
+							if (isset($_REQUEST['acao']))
+							{
+								if ($_REQUEST['acao'] == 'novo') {
+									?>
+										<form>
+											<label>Código</label><br />
+											<input type="text" name="txtCodigo" value="" /><br />
+											<label>Nome</label><br />
+											<input type="text" name="txtNome" value="" /><br />
+											<label>Valor</label><br />
+											<input type="text" name="txtValor" value="" /><br />
+											<label>Descrição</label><br />
+											<textarea rows="4" cols="30" name="txtDescricao"></textarea><br />
+											<input type="submit" name="btnSalvar" value="Salvar" />							
+										</form>
+									<?
+								}
+							}
+						}
+        			?>
+        		</td>
+        	</tr>
+        </table>        
+    </body>
+</html>
